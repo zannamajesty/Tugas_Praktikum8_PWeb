b1cd6c70c29f92707eb18c8630d6365b432cd203
zannamajesty <75726721+zannamajesty@users.noreply.github.com>
2021-05-03 19:03:49 +0700

    Add files via upload

---
+++ database.php
@@ -0,0 +1,9 @@
+<?php //perintah php
+// konfigurasi database
+$host="localhost";
+$user="root";
+$password="";
+$database="form_pendaftaran";
+// perintah php untuk mengakses database
+$koneksi=mysqli_connect($host, $user, $password, $database);
+?>
